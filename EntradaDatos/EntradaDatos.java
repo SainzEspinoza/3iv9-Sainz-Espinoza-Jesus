3d023e679ba1885d500be5c078f7c469fee8162e
Jesus-Sainz <sainz.espinoza.jesus.jair@gmail.com>
2020-10-20 00:15:46 -0500

    Entrada Datos siii

---
+++ EntradaDatos/EntradaDatos.java
@@ -0,0 +1,14 @@
+class EntradaDatos{
+    /* Este programa nos va a servir para
+    poder meter datos */
+
+    public static void main(String[] args){
+        String nombre;
+        System.out.println("Para Ir al infierno escribe tu nombre");
+        //leer
+        //Entrada estandar de la computadora
+        nombre = System.console().readLine();
+        System.out.println("HOLA: "+ nombre + " al Infierno, mua ja ja");
+    }
+
+}
