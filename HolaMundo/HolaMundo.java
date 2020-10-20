35b3f2abfe29a2f6ea60cb26d1204c95e4b955a0
Jesus-Sainz <sainz.espinoza.jesus.jair@gmail.com>
2020-10-19 21:37:30 -0500

    Hola Mundo

---
+++ HolaMundo/HolaMundo.java
@@ -0,0 +1,25 @@
+class HolaMundo{
+
+    //Para poder hacer un comentario sobre la linea de codigo
+    /*
+    para poder realizar comentarios
+    en diferentes lineas de codigo
+    y asi escribir todo lo que querramos y no afectara
+    al codigo principal */
+    class classname{
+        /*es el cuerpo del programa, aqui van todas las instrucciones
+        operaciones, metodos, variables, etc.
+        */
+    }
+      public static void main(String[] args){
+          //una declaracion,una idea??, orden
+          //estructura base de cualquer clase principal
+
+          //inicializacion de variables
+          String name = "Jesus Christ our Lord";
+          //escribir "la oracion"
+          //clase.objeto.metodo
+          System.out.println("Say Hello World or Die");
+          System.out.println(name);
+      }
+    }
